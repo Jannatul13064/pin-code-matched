31464746e3068f7707b9df2496acbaccf1efad95
Akash Islam <akzzislam19194@gmail.com>
2021-08-20 15:14:15 -0500

    Thirdtime

--- js/app.js
+++ js/app.js
@@ -18,7 +18,29 @@ function generatePin() {
 document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = (event.target.innerText);
     const calcInput = document.getElementById('typed-numbers');
-    const previousNumber = calcInput.value;
-    const newCalc = previousNumber + number;
-    calcInput.value = newCalc;
-})
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+});
+function varifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
