380536beea324d013e523990797741b73211ad9a
Akash Islam <akzzislam19194@gmail.com>
2021-08-20 14:29:36 -0500

    second

--- js/app.js
+++ js/app.js
@@ -14,3 +14,11 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = (event.target.innerText);
+    const calcInput = document.getElementById('typed-numbers');
+    const previousNumber = calcInput.value;
+    const newCalc = previousNumber + number;
+    calcInput.value = newCalc;
+})
